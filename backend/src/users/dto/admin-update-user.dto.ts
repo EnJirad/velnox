ff1f52e2017362893d48f37499c2ff2cc76077b3
EnJirad <ma0979354966.js91934@gmail.com>
2026-08-04 00:06:39 +0700

    update2

--- backend/src/users/dto/admin-update-user.dto.ts
+++ backend/src/users/dto/admin-update-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsEnum, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsEmail, IsEnum, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class AdminUpdateUserDto {
   @IsOptional()
