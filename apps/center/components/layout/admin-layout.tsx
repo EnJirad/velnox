032af4e32956c3283cdfaad557b5533e87f38bcb
EnJirad <ma0979354966.js91934@gmail.com>
2026-08-07 23:27:29 +0700

    update2

--- apps/center/components/layout/admin-layout.tsx
+++ apps/center/components/layout/admin-layout.tsx
@@ -41,14 +41,6 @@ function IconStore({ className }: { className?: string }) {
   );
 }
 
-function IconShop({ className }: { className?: string }) {
-  return (
-    <svg className={className} width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path d="M3 21V8l9-5 9 5v13" />
-      <path d="M7 21v-8h10v8" />
-    </svg>
-  );
-}
 
 function IconBox({ className }: { className?: string }) {
   return (
